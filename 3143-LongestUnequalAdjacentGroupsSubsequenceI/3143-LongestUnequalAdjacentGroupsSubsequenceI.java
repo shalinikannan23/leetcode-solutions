1c97242eb52ef50da0ab4325736b8a2d4147147b
Shalinikannan <shalinikannan2307@gmail.com>
2025-09-16 14:16:29 +0530

    Create 3143-LongestUnequalAdjacentGroupsSubsequenceI

---
+++ 3143-LongestUnequalAdjacentGroupsSubsequenceI/3143-LongestUnequalAdjacentGroupsSubsequenceI.java
@@ -0,0 +1,14 @@
+// Last updated: 9/16/2025, 2:16:27 PM
+class Solution {
+    public List<String> getLongestSubsequence(String[] words, int[] groups) {
+        List<String> result = new ArrayList<>();
+        int last = -1;
+        for (int i = 0; i < words.length; i++) {
+            if (groups[i] != last) {
+                result.add(words[i]);
+                last = groups[i];
+            }
+        }
+        return result;
+    }
+}
