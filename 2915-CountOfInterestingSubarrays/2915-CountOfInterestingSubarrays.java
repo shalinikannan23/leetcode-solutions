683d3477588a060dd0b32930f5043e44868aaad0
Shalinikannan <shalinikannan2307@gmail.com>
2025-09-16 14:16:42 +0530

    Create 2915-CountOfInterestingSubarrays

---
+++ 2915-CountOfInterestingSubarrays/2915-CountOfInterestingSubarrays.java
@@ -0,0 +1,19 @@
+// Last updated: 9/16/2025, 2:16:40 PM
+class Solution {
+    public long countInterestingSubarrays(List<Integer> nums, int m, int k) {
+        long total = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int count = 0;
+
+        for (int num : nums) {
+            if (num % m == k) count++;
+            int remainder = (count - k) % m;
+            if (remainder < 0) remainder += m;
+            total += map.getOrDefault(remainder, 0);
+            map.put(count % m, map.getOrDefault(count % m, 0) + 1);
+        }
+
+        return total;
+    }
+}
